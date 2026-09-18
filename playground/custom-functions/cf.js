98904222b4272e003a04c3d14891372091c69212
BabarZargar <babarzargar95@gmail.com>
2026-09-18 19:25:24 +0530

    code for creating functions

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,9 @@
+function calculateBill(){
+    console.log("Running Calculate Bill!");
+    const total = 100*1.13; 
+    return total;
+}
+
+const myTotal = calculateBill();
+console.log(`Your total is ₹${myTotal}`);
+console.log(`Your total is ₹${calculateBill()}`); //interpolation
